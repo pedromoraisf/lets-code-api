4e1300e92e6efb5c88b1117e0be9fffd5b0b3047
pedromoraisf <pedro.morais1997@gmail.com>
2021-11-29 22:47:38 -0300

    refactor: clarify specification name

--- src/hexagon/usecases/create-kanban-card/create-kanban-card.spec.ts
+++ src/hexagon/usecases/create-kanban-card/create-kanban-card.spec.ts
@@ -1,11 +1,11 @@
-describe('Create Card Use Case', () => {
+describe('Create Kanban Card Use Case', () => {
   test.todo('should validate that title, content and list are filled')
 
-  describe('Create card repository', () => {
+  describe('Create kanban card repository', () => {
     test.todo('should call repository method correctly')
 
     test.todo('should throw an error if repository throw a low-level error')
   })
 
-  test.todo('should return the created card if operation dont have errors')
+  test.todo('should return the created kanban card if operation dont have errors')
 })
