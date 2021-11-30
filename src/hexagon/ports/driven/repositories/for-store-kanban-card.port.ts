4afdef5a9fe70697ba64181592c6a83a0263d7b4
pedromoraisf <pedro.morais1997@gmail.com>
2021-11-30 20:12:25 -0300

    feat: add port i/o

--- src/hexagon/ports/driven/repositories/for-store-kanban-card.port.ts
+++ src/hexagon/ports/driven/repositories/for-store-kanban-card.port.ts
@@ -1,9 +1,11 @@
+import { KanbanCardDto } from '@src/hexagon/entities'
+
 export interface ForStoreKanbanCardPort {
   storeKanbanCard: (data: ForStoreKanbanCardPort.Params) => Promise<ForStoreKanbanCardPort.Result>
 }
 
 export namespace ForStoreKanbanCardPort {
-  export type Params = {}
+  export type Params = Omit<KanbanCardDto, 'id'>
 
-  export type Result = {}
+  export type Result = KanbanCardDto
 }
