a5f1675e601f47f3da1c78ce2cb17e7810e96121
pedromoraisf <pedro.morais1997@gmail.com>
2021-11-29 22:20:17 -0300

    test: add create card sketch

---
+++ src/hexagon/usecases/create-card/create-card.spec.ts
@@ -0,0 +1,11 @@
+describe('Create Card Use Case', () => {
+  test.todo('should validate that title, content and list are filled')
+
+  describe('Create card repository', () => {
+    test.todo('should call repository method correctly')
+
+    test.todo('should throw an error if repository throw a low-level error')
+  })
+
+  test.todo('should return the created card if operation dont have errors')
+})
