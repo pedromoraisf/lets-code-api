b927f416548c8ac910315a41711e016cb5525073
pedromoraisf <pedro.morais1997@gmail.com>
2021-11-30 21:11:34 -0300

    feat: add update kanban card in memory adapter

--- src/adapters/db/in-memory/kanban-card-repository/kanban-card-repository-in-memory.adapter.ts
+++ src/adapters/db/in-memory/kanban-card-repository/kanban-card-repository-in-memory.adapter.ts
@@ -1,7 +1,11 @@
-import { ForStoreKanbanCardPort } from '@src/hexagon/ports/driven'
+/* eslint-disable @typescript-eslint/indent */
+/* eslint-disable @typescript-eslint/brace-style */
+import { ForStoreKanbanCardPort, ForUpdateKanbanCardPort } from '@src/hexagon/ports/driven'
 import { KanbanCard } from '@src/hexagon/entities'
 
-export class KanbanCardRepositoryAdapterInMemory implements ForStoreKanbanCardPort {
+export class KanbanCardRepositoryAdapterInMemory
+  implements ForStoreKanbanCardPort, ForUpdateKanbanCardPort
+{
   private readonly _kanbanCards: any[] = []
 
   async storeKanbanCard(
@@ -12,5 +16,15 @@ export class KanbanCardRepositoryAdapterInMemory implements ForStoreKanbanCardPo
     this._kanbanCards.push(kanbanCard)
 
     return await Promise.resolve(kanbanCard)
+  }
+
+  async updateKanbanCard(
+    data: ForUpdateKanbanCardPort.Params
+  ): Promise<ForUpdateKanbanCardPort.Params> {
+    const kanbanCardPosition = this._kanbanCards.findIndex(persisted => persisted.id === data.id)
+
+    this._kanbanCards.splice(kanbanCardPosition, 1, data)
+
+    return await Promise.resolve(data)
   }
 }
