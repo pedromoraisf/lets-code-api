dd5025e4e96a0939058618524b9d35b7987db586
pedromoraisf <pedro.morais1997@gmail.com>
2021-12-01 20:17:11 -0300

    feat: exposes list kanban cards api

--- src/startup/setup/routes.ts
+++ src/startup/setup/routes.ts
@@ -3,11 +3,13 @@ import { Express } from 'express'
 import {
   makeAuthenticationExpressAdapter,
   makeChangeKanbanCardExpressAdapter,
-  makeCreateKanbanCardExpressAdapter
+  makeCreateKanbanCardExpressAdapter,
+  makeListKanbanCardsExpressAdapter
 } from '@src/startup/factories/adapters/http'
 
 export const setupRoutes = (app: Express) => {
   app.post('/login', makeAuthenticationExpressAdapter().handle)
   app.post('/cards', makeCreateKanbanCardExpressAdapter().handle)
   app.put('/cards/:id', makeChangeKanbanCardExpressAdapter().handle)
+  app.get('/cards', makeListKanbanCardsExpressAdapter().handle)
 }
