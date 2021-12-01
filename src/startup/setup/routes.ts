2f277cb54ec2ddeb223af47dd3f7c1bf8db77efb
pedromoraisf <pedro.morais1997@gmail.com>
2021-12-01 20:07:47 -0300

    feat: expose update card api

--- src/startup/setup/routes.ts
+++ src/startup/setup/routes.ts
@@ -2,10 +2,12 @@
 import { Express } from 'express'
 import {
   makeAuthenticationExpressAdapter,
+  makeChangeKanbanCardExpressAdapter,
   makeCreateKanbanCardExpressAdapter
 } from '@src/startup/factories/adapters/http'
 
 export const setupRoutes = (app: Express) => {
   app.post('/login', makeAuthenticationExpressAdapter().handle)
   app.post('/cards', makeCreateKanbanCardExpressAdapter().handle)
+  app.put('/cards/:id', makeChangeKanbanCardExpressAdapter().handle)
 }
