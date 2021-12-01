210bc21fb018e708d9013230fd3899cc2fd21b07
pedromoraisf <pedro.morais1997@gmail.com>
2021-11-30 23:30:12 -0300

    chore: add babel with build compiler

---
+++ babel.config.js
@@ -0,0 +1,28 @@
+/* eslint-disable no-undef */
+module.exports = {
+  presets: [
+    [
+      '@babel/preset-env',
+      {
+        targets: {
+          node: '15'
+        }
+      }
+    ],
+    '@babel/preset-typescript'
+  ],
+  plugins: [
+    ['@babel/plugin-proposal-class-properties'],
+    [
+      'module-resolver',
+      {
+        alias: {
+          '@src': './src',
+          '@tests': './tests'
+        }
+      }
+    ],
+    ['add-module-exports']
+  ],
+  ignore: ['**/*.spec.ts', '**/*.test.ts']
+}
