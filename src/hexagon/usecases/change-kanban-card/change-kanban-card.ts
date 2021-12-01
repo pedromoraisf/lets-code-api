f6886b8415350df1e372abeec756a24862f20bcf
pedromoraisf <pedro.morais1997@gmail.com>
2021-11-30 21:02:54 -0300

    refactor: extract method

--- src/hexagon/usecases/change-kanban-card/change-kanban-card.ts
+++ src/hexagon/usecases/change-kanban-card/change-kanban-card.ts
@@ -1,19 +1,14 @@
 import { ForChangeKanbanCardPort } from '@src/hexagon/ports/driver'
+import { validateObjectProperties } from '@src/hexagon/usecases/utils'
 
 export class ChangeKanbanCardUseCase implements ForChangeKanbanCardPort {
   async execute(data: ForChangeKanbanCardPort.Params): Promise<ForChangeKanbanCardPort.Result> {
-    if (this.validate(data)) {
+    if (this.haveFalsyProperty(data)) {
       throw new Error()
     }
 
     return await Promise.resolve({} as any)
   }
 
-  private validate(data: ForChangeKanbanCardPort.Params): boolean {
-    const requiredProps = ['id', 'title', 'content', 'list']
-
-    const falsyProperty = (prop: string) => !data[prop]
-
-    return requiredProps.some(falsyProperty)
-  }
+  private readonly haveFalsyProperty = validateObjectProperties(['id', 'title', 'content', 'list'])
 }
