9e54993781a6029c8e9cfe92f25b6ea5699f6607
pedromoraisf <pedro.morais1997@gmail.com>
2021-11-30 21:14:52 -0300

    feat: add call repository method correctly

--- src/hexagon/usecases/change-kanban-card/change-kanban-card.ts
+++ src/hexagon/usecases/change-kanban-card/change-kanban-card.ts
@@ -1,11 +1,16 @@
+import { ForUpdateKanbanCardPort } from '@src/hexagon/ports/driven'
 import { ForChangeKanbanCardPort } from '@src/hexagon/ports/driver'
 import { validateObjectProperties } from '@src/hexagon/usecases/utils'
 
 export class ChangeKanbanCardUseCase implements ForChangeKanbanCardPort {
+  constructor(private readonly forUpdateKanbanCard: ForUpdateKanbanCardPort) {}
+
   async execute(data: ForChangeKanbanCardPort.Params): Promise<ForChangeKanbanCardPort.Result> {
     if (this.haveFalsyProperty(data)) {
       throw new Error()
     }
+
+    await this.forUpdateKanbanCard.updateKanbanCard(data)
 
     return await Promise.resolve({} as any)
   }
