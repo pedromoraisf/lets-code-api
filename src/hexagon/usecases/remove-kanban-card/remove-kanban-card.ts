ffcbcca255288e42b24cd6999022b499cf5847ee
pedromoraisf <pedro.morais1997@gmail.com>
2021-11-30 22:04:04 -0300

    feat: add call repository method correctly

--- src/hexagon/usecases/remove-kanban-card/remove-kanban-card.ts
+++ src/hexagon/usecases/remove-kanban-card/remove-kanban-card.ts
@@ -1,11 +1,16 @@
+import { ForDeleteKanbanCardPort } from '@src/hexagon/ports/driven'
 import { ForRemoveKanbanCardPort } from '@src/hexagon/ports/driver'
 import { validateObjectProperties } from '@src/hexagon/usecases/utils'
 
 export class RemoveKanbanCardUseCase implements ForRemoveKanbanCardPort {
+  constructor(private readonly forDeleteKanbanCardPort: ForDeleteKanbanCardPort) {}
+
   async execute(data: ForRemoveKanbanCardPort.Params): Promise<ForRemoveKanbanCardPort.Result> {
     if (this.haveFalsyProperty(data)) {
       throw new Error()
     }
+
+    await this.forDeleteKanbanCardPort.deleteKanbanCard(data)
 
     return await Promise.resolve([] as any)
   }
