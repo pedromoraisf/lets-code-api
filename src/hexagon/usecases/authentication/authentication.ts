90bd34a423d614a1f31ad5e87497215bdf683216
pedromoraisf <pedro.morais1997@gmail.com>
2021-11-30 23:02:13 -0300

    feat: add call generate encrypted token correctly

--- src/hexagon/usecases/authentication/authentication.ts
+++ src/hexagon/usecases/authentication/authentication.ts
@@ -1,11 +1,20 @@
 import { ForFindUserPort } from '@src/hexagon/ports/driven'
+import { ForGenerateEncryptedCodePort } from '@src/hexagon/ports/driven/crypto'
 import { ForAuthenticationPort } from '@src/hexagon/ports/driver'
 
 export class AuthenticationUseCase implements ForAuthenticationPort {
-  constructor(private readonly forFindUserPort: ForFindUserPort) {}
+  constructor(
+    private readonly forFindUserPort: ForFindUserPort,
+    private readonly forGenerateEncryptedCodePort: ForGenerateEncryptedCodePort
+  ) {}
 
   async execute(data: ForAuthenticationPort.Params): Promise<ForAuthenticationPort.Result> {
-    await this.forFindUserPort.findUser(data)
+    const user = await this.forFindUserPort.findUser(data)
+
+    this.forGenerateEncryptedCodePort.generateEncryptedCode({
+      lifetime: 3600,
+      toEncrypt: user
+    })
 
     return await Promise.resolve({} as any)
   }
