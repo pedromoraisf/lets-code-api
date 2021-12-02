930c29dd9ae6d24a4f6d6ce073fd17992a5cb550
pedromoraisf <pedro.morais1997@gmail.com>
2021-12-01 22:14:08 -0300

    feat: return error object with 400 status code if operation have client error

--- src/adapters/http/express/create-kanban-card-express.adapter.ts
+++ src/adapters/http/express/create-kanban-card-express.adapter.ts
@@ -1,16 +1,23 @@
 import { Request, Response } from 'express'
 import { CreateKanbanCardUseCase } from '@src/hexagon/usecases'
+import { InvalidParamsError } from '@src/hexagon/usecases/errors'
 
 export class CreateKanbanCardExpressAdapter {
   constructor(private readonly createKanbanCardUseCase: CreateKanbanCardUseCase) {}
 
   handle = async ({ body, params, query }: Request, res: Response) => {
-    const result = await this.createKanbanCardUseCase.execute({
-      ...body,
-      ...params,
-      ...query
-    })
+    try {
+      const result = await this.createKanbanCardUseCase.execute({
+        ...body,
+        ...params,
+        ...query
+      })
 
-    return res.status(202).send(result)
+      return res.status(202).send(result)
+    } catch (error) {
+      if (error instanceof InvalidParamsError) {
+        return res.status(400).send(error.message)
+      }
+    }
   }
 }
