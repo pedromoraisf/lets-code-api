e1f5084d46ce75c79d43c2ce2666aa6b732a1f71
pedromoraisf <pedro.morais1997@gmail.com>
2021-12-01 23:41:00 -0300

    test: ensure return error object with 500~ status code if operation have server error

--- tests/integration/adapters/http/change-kanban-card-express-adapter.test.ts
+++ tests/integration/adapters/http/change-kanban-card-express-adapter.test.ts
@@ -79,5 +79,18 @@ describe('Change Kanban Card Express Adapter', () => {
     })
   })
 
-  test.todo('should return error object with 500~ status code if operation have server error')
+  test('should return error object with 500~ status code if operation have server error', async () => {
+    const { sut, kanbanCardRepositoryInMemoryAdapter } = makeSut()
+
+    jest
+      .spyOn(kanbanCardRepositoryInMemoryAdapter, 'updateKanbanCard')
+      .mockReturnValueOnce(Promise.reject(new Error('any_low_level_error')))
+
+    const testable = await sut.handle(makeFixture(), makeExpressResponseMock())
+
+    expect(testable).toEqual({
+      statusCode: 500,
+      result: 'server error'
+    })
+  })
 })
