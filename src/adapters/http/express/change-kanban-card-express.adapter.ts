5a23e84609d351ecddc593915859b767e7e1b107
pedromoraisf <pedro.morais1997@gmail.com>
2021-12-01 23:36:59 -0300

    feat: add return error object with 404 status code if operation receive not found id

--- src/adapters/http/express/change-kanban-card-express.adapter.ts
+++ src/adapters/http/express/change-kanban-card-express.adapter.ts
@@ -1,16 +1,23 @@
 import { Request, Response } from 'express'
 import { ChangeKanbanCardUseCase } from '@src/hexagon/usecases'
+import { InvalidIdProvidedError } from '@src/hexagon/usecases/errors'
 
 export class ChangeKanbanCardExpressAdapter {
   constructor(private readonly changeKanbanCardUseCase: ChangeKanbanCardUseCase) {}
 
   handle = async ({ body, params, query }: Request, res: Response) => {
-    const result = await this.changeKanbanCardUseCase.execute({
-      ...body,
-      ...params,
-      ...query
-    })
+    try {
+      const result = await this.changeKanbanCardUseCase.execute({
+        ...body,
+        ...params,
+        ...query
+      })
 
-    return res.status(200).send(result)
+      return res.status(200).send(result)
+    } catch (error) {
+      if (error instanceof InvalidIdProvidedError) {
+        return res.status(404).send(error.message)
+      }
+    }
   }
 }
