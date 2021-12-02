128759ac86d6ee2757639d52fabd749fa87c18f5
pedromoraisf <pedro.morais1997@gmail.com>
2021-12-01 21:34:12 -0300

    feat: inject jwt dependency

--- src/startup/factories/usecases/authentication-usecase.factory.ts
+++ src/startup/factories/usecases/authentication-usecase.factory.ts
@@ -1,9 +1,6 @@
 import { AuthenticationUseCase } from '@src/hexagon/usecases/authentication'
-import { makeGenerateCryptoCodeMockAdapter } from '@src/startup/factories/adapters/crypto'
+import { makeGenerateCryptoCodeJwtAdapter } from '@src/startup/factories/adapters/crypto'
 import { makeUserRepositoryInMemoryAdapter } from '@src/startup/factories/adapters/db'
 
 export const makeAuthenticationUseCase = () =>
-  new AuthenticationUseCase(
-    makeUserRepositoryInMemoryAdapter(),
-    makeGenerateCryptoCodeMockAdapter()
-  )
+  new AuthenticationUseCase(makeUserRepositoryInMemoryAdapter(), makeGenerateCryptoCodeJwtAdapter())
