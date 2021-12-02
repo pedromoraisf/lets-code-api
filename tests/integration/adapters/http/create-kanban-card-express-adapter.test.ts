bc6dc9ba494e58922c4985fb30646f726e089b06
pedromoraisf <pedro.morais1997@gmail.com>
2021-12-01 22:10:59 -0300

    test: return success object with 202 status code if operation dont have any error

---
+++ tests/integration/adapters/http/create-kanban-card-express-adapter.test.ts
@@ -0,0 +1,65 @@
+import { KanbanCardRepositoryInMemoryAdapter } from '@src/adapters/db'
+import { CreateKanbanCardExpressAdapter } from '@src/adapters/http'
+import { ForStoreKanbanCardPort } from '@src/hexagon/ports/driven'
+import { CreateKanbanCardUseCase } from '@src/hexagon/usecases'
+
+const makeFixture = ({
+  title = 'any_title',
+  content = 'any_content',
+  list = 'any_list'
+}: any = {}): any => ({
+  body: {
+    title,
+    content,
+    list
+  },
+  params: {},
+  query: {}
+})
+
+const makeExpressResponseMock = (): any => ({
+  status: statusCode => ({
+    send: result => ({
+      statusCode,
+      result
+    })
+  })
+})
+
+type SutTypes = {
+  sut: CreateKanbanCardExpressAdapter
+  kanbanCardRepositoryInMemoryAdapter: ForStoreKanbanCardPort
+}
+
+const makeSut = (): SutTypes => {
+  const kanbanCardRepositoryInMemoryAdapter = new KanbanCardRepositoryInMemoryAdapter()
+  const createKanbanCardUseCase = new CreateKanbanCardUseCase(kanbanCardRepositoryInMemoryAdapter)
+
+  const sut = new CreateKanbanCardExpressAdapter(createKanbanCardUseCase)
+
+  return {
+    sut,
+    kanbanCardRepositoryInMemoryAdapter
+  }
+}
+
+describe('Create Kanban Card Express Adapter', () => {
+  test('should return success object with 202 status code if operation dont have any error', async () => {
+    const { sut } = makeSut()
+
+    const testable = await sut.handle(makeFixture(), makeExpressResponseMock())
+
+    expect(testable).toEqual({
+      statusCode: 202,
+      result: expect.objectContaining({
+        content: 'any_content',
+        list: 'any_list',
+        title: 'any_title'
+      })
+    })
+  })
+
+  test.todo('should return error object with 400 status code if operation have client error')
+
+  test.todo('should return error object with 500~ status code if operation have server error')
+})
